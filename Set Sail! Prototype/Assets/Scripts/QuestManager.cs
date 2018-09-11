7ee4ec921ac4e5ce96f16b9856debf91daca2243
Ian van Heeren <ianvanheeren@hotmail.com>
2018-09-11 15:03:35 +0200

    sorry ik kom ff tussendoor mijnheren

---
+++ Set Sail! Prototype/Assets/Scripts/QuestManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestManager : MonoBehaviour {
+
+	private GameObject questTarget;
+	private bool questActive = false;
+
+	[SerializeField]
+	private GameObject questParent;
+	[SerializeField]
+	private Text questHeadline;
+	[SerializeField]
+	private Text questSubtext;
+
+	private string vesselHeadline = "Vessel Spotted!";
+	private string vesselMessage = "Travel towards a vessel to the % of you.";
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(questActive)
+		{
+			Debug.Log("Quest is active");
+			if(questTarget == null)
+			{
+				StopQuest();
+				return;
+			}
+
+			// Got to replace the message in a local variable cus we no change template
+			string theSubtext = vesselMessage;
+			questSubtext.text = theSubtext.Replace("%", DirectionToTarget());
+		}
+	}
+
+	public void StartQuest(GameObject newQuestTarget)
+	{
+		questActive = true;
+		questTarget = newQuestTarget;
+
+		questHeadline.text = vesselHeadline;
+		questSubtext.text = vesselMessage + DirectionToTarget();
+		questParent.SetActive(true);
+	}
+
+	public void StopQuest()
+	{
+		if(questActive)
+		{
+			questActive = false;
+			questTarget = null;
+			questParent.SetActive(false);
+		}
+	}
+
+	private string DirectionToTarget()
+	{
+		string direction = "";
+
+		// North/South
+		if(questTarget.transform.position.z >= transform.position.z)
+			direction += "north";
+		else
+			direction += "south";
+
+		if(questTarget.transform.position.x >= transform.position.x)
+			direction += "east";
+		else
+			direction += "west";
+
+		return direction;
+	}
+}
---
+++ Set Sail! Prototype/Assets/Scripts/QuestTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestTrigger : MonoBehaviour {
+
+	public float speedThreshold = 0.5f;
+	public GameObject prefabQuestWindow;
+	public Transform sceneCanvas;
+	private bool _activated = false;
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.tag == "Player" && !_activated)
+		{
+			if(other.gameObject.transform.GetComponent<ShipControls>().sailVelocity < speedThreshold)
+			{
+				_activated = true;
+				GameObject questWindow = Instantiate(prefabQuestWindow);
+				if(questWindow != null)
+				{
+					questWindow.SetActive(true);
+					questWindow.transform.SetParent(sceneCanvas);
+
+					// Change quest windows text
+					questWindow.GetComponent<QuestWindow>().UpdateText("Destroy Enemy Vessel", "Placeholder text here. When accepting ship will spawn.");
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		Debug.Log("Ship exits QuestTrigger");
+		_activated = false;
+	}
+}

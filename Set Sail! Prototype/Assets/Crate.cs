353c744aaddedb1b4bc7f40c034d9500c343028d
Tom <halo3halo@hotmail.com>
2018-09-11 16:30:40 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Set Sail! Prototype/Assets/Crate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour {
+    public float waterHeight = 0;
+    public float upForce = 10f;
+
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            // Pick up crate
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        float upwards = waterHeight - transform.position.y;
+        if (upwards > 0)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * upwards / upForce);
+        }
+    }
+}
